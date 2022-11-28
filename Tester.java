782e6cc7c5a574ffae379be9d3a9d11804412158
AmirArmion <A_R_Armion@Yahoo.co.uk>
2022-11-27 19:02:56 -0800

    stage01

---
+++ Tester.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+/**
+* 
+* @author: Amir Armion 
+* 
+* @version: V.01
+* 
+*/
+public class Tester
+{
+    public void testLogEntry() 
+    {
+        LogEntry le = new LogEntry("1.2.3.4", new Date(), "example request", 200, 500);
+        System.out.println(le);
+        
+        LogEntry le2 = new LogEntry("1.2.100.4", new Date(), "example request 2", 300, 400);
+        System.out.println(le2);
+    }
+    
+    public void testLogAnalyzer() 
+    {
+        // complete method
+    }
+}
